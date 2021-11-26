044dd04725c543ed8daa17e5d6cbee303e0bac79
daw2-LopezCarod22 <91276534+daw2-LopezCarod22@users.noreply.github.com>
2021-11-26 19:46:33 +0100

    Add files via upload

---
+++ N2P8Comments.php
@@ -0,0 +1,21 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>N2P1</title>
+</head>
+<body>
+    <p>Create a list of the 10 most important concepts about what you’ve learned in this document.</p>
+    <ul>
+        <li>He aprendido a añadir mensajes de error a la hora de añadir y editar usuarios</li>
+    </ul>
+    <p>Qualifies the document (0-10), and the teacher's explanation (0-10).<br></p>
+    <p>Document: 9<br></p>
+    <p>Teacher explanation: 9<br></p>
+    <p>As a student, put a mark to yourself.<br></p>
+    <p>Myself mark: 9<br></p>
+    <p>Explain an improvement.<br></p>
+    <p>Ha sido facil del seguir.</p>
+</body>
+</html>
